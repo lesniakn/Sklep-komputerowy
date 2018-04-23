69155660ea4a62f2b8d1d2ce4fc6a52c49548750
Norbert Leśniak <norbert.lesniak@gmail.com>
2018-04-23 12:10:06 +0200

    Dodanie atrybutów do klas

--- Sklep/Sklep/Models/Kategoria.cs
+++ Sklep/Sklep/Models/Kategoria.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,11 @@ namespace Sklep.Models
     public class Kategoria
     {
         public int KategoriaId { get; set; }
+        [Required(ErrorMessage = "Wprowadź nazwę kategorii")]
+        [StringLength(50)]
         public string NazwaKategorii { get; set; }
+        [Required(ErrorMessage = "Wprowadź opis kategorii")]
+
         public string OpisKategorii { get; set; }
         public string NazwaPlikuIkony { get; set; }
 
--- Sklep/Sklep/Models/Przedmiot.cs
+++ Sklep/Sklep/Models/Przedmiot.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,9 +10,17 @@ namespace Sklep.Models
     {
         public int PrzedmiotId { get; set; }
         public int KategoriaId { get; set; }
+
+        [Required(ErrorMessage = "Wprowadź nazwę przedmiotu")]
+        [StringLength(50)]
         public string NazwaPrzedmiotu { get; set; }
+
+        [Required(ErrorMessage = "Wprowadź producenta przedmiotu")]
+        [StringLength(50)]
         public string ProducentPrzedmiotu { get; set; }
         public DateTime DataDodania { get; set; }
+
+        [StringLength(100)]
         public string NazwaPlikuObrazka { get; set; }
         public string OpisPrzedmiotu { get; set; }
         public decimal CenaPrzedmiotu { get; set; }
