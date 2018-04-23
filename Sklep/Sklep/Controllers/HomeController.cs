df70730b73cec32f72eea70b10538b3b21ddd4f9
Norbert Leśniak <norbert.lesniak@gmail.com>
2018-04-23 13:04:09 +0200

    Pierwszy test bazy danych

--- Sklep/Sklep/Controllers/HomeController.cs
+++ Sklep/Sklep/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Sklep.DAL;
+using Sklep.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +10,30 @@ namespace Sklep.Controllers
 {
     public class HomeController : Controller
     {
+        private PrzedmiotContext db = new PrzedmiotContext();
+
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+            Kategoria kategoria = new Kategoria { NazwaKategorii = "Płyty główne", NazwaPlikuIkony = "plytyglowne.png", OpisKategorii = "opis" };
+            db.Kategorie.Add(kategoria);
+            db.SaveChanges();
 
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+            public ActionResult About()
+            {
+                ViewBag.Message = "Your application description page.";
 
-            return View();
+                return View();
+            }
+
+            public ActionResult Contact()
+            {
+                ViewBag.Message = "Your contact page.";
+
+                return View();
+            }
         }
     }
-}
+
